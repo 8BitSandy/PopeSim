f4b19bd884573d6a8b7880ad0937b936cc8f802c
cj1m <cj1mckay@gmail.com>
2016-01-20 21:18:46 +0000

    Added memes

--- app/src/main/java/com/example/sandy/popesimulator/MainActivity.java
+++ app/src/main/java/com/example/sandy/popesimulator/MainActivity.java
@@ -1,28 +1,28 @@
 package com.example.sandy.popesimulator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+
 
 public class MainActivity extends AppCompatActivity {
-    private int money = 0;
-    private int incrementRate = 1;
-    TextView tView;
+    //Using big decimal because Java is incompetent of adding simple decimals
+    private BigDecimal money = new BigDecimal("0");
+    private BigDecimal incrementRate = new BigDecimal("0.01");
+    private TextView moneyTView;
+    private TextView splashTView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        tView = (TextView) findViewById(R.id.textView);
-        setSupportActionBar(toolbar);
+        moneyTView = (TextView) findViewById(R.id.moneyText);
+        splashTView = (TextView) findViewById(R.id.splashText);
     }
 
     @Override
@@ -48,8 +48,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //@Override
-    public void test(View v){
-        money += incrementRate;
-        tView.setText("£"+money);
+    public void makeMoney(View v){
+        money = money.add(incrementRate);
+        moneyTView.setText("£" + money);
+        splashTView.setText(SplashTextMemer.getSplashText());
     }
 }
---
+++ app/src/main/java/com/example/sandy/popesimulator/SplashTextMemer.java
@@ -0,0 +1,14 @@
+package com.example.sandy.popesimulator;
+
+/**
+ * Created by Christopher on 20/01/2016.
+ */
+public class SplashTextMemer {
+    private static String[] memes = {"Moses is bae - Hebrews the best coffee","Are you Abel to pray?","Prayz 4 Dayz","So broke call me St. Nickeless","Hoping for a good poping", "To the pope-mobile!", "Get busy prayin, or get busy dieing","These nuns ain't loyal","#GodRapedMary","Marry had a little bam!"};
+
+    public static String getSplashText(){
+        int random = (int) Math.round(Math.random()* (memes.length-1));
+
+        return memes[random];
+    }
+}
