783b99f2424325750870487ffd6676c1ef04b36b
8BitSandy <8bitsandy@gmail.com>
2016-01-20 20:02:23 +0000

    sometin sometin sometin DARK SIDE

--- app/src/main/java/com/example/sandy/popesimulator/MainActivity.java
+++ app/src/main/java/com/example/sandy/popesimulator/MainActivity.java
@@ -8,24 +8,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    int Money = 0;
+    String Moneyword;
+    View tView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tView = (TextView) findViewById(R.id.textView);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +46,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //@Override
+    public void test(View v){
+        Money += 1;
+        tView.setText(Moneyword);
     }
 }
